c10cef415d63aa5715b4496548236558c4ca5322
kevbar6 <45368793+kevbar6@users.noreply.github.com>
2019-02-19 19:29:13 +0100

    Update BlockSaplingBase.java

--- java/kevbar6/portalgun/objects/blocks/BlockSaplingBase.java
+++ java/kevbar6/portalgun/objects/blocks/BlockSaplingBase.java
@@ -1,6 +1,7 @@
 package kevbar6.portalgun.objects.blocks;
 
 import kevbar6.portalgun.Main;
+import kevbar6.portalgun.init.BlockInit;
 import kevbar6.portalgun.init.ItemInit;
 import kevbar6.portalgun.util.interfaces.IHasModel;
 import kevbar6.portalgun.world.generation.BWoodTree;
@@ -37,6 +38,7 @@ public class BlockSaplingBase extends BlockBush implements IGrowable, IHasModel
         setHardness(0.0F);
         setSoundType(SoundType.PLANT);
 
+        BlockInit.BLOCKS.add(this);
         ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
     }
 
@@ -117,6 +119,6 @@ public class BlockSaplingBase extends BlockBush implements IGrowable, IHasModel
 
     @Override
     public void registerModels() {
-        Main.proxy.registerModel(Item.getItemFromBlock(this), 0);
+        Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
     }
 }
